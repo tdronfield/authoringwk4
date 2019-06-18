ad9ac73a44e980a3bd255d671bbc38503b18ae63
tdronfield <t_dronfield@fanshaweonline.ca>
2019-06-18 16:55:19 -0400

    adding click and change function to different bg images

--- js/app.js
+++ js/app.js
@@ -1,62 +1,79 @@
 (() => {
-  //stub
-  console.log('fired!');
+	// set up the puzzle pieces and boards
+	//
+	// const is short for constant -> variables that shouldn't change
+	const piecesBoard = document.querySelector(".puzzle-pieces"),
+		puzzleBoard = document.querySelector(".puzzle-board"),
+		puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
+		dropZones = document.querySelectorAll(".drop-zones");
 
-  //cont is short for constant -> variables that shouldn't change
-  const piecesBoard = document.querySelector('.puzzle-pieces'),
-        puzzleBoard = document.querySelector('.puzzle-board'),
-        puzzleSelectors = document.querySelectorAll('#buttonHolder img'),
-        dropZones = document.querySelectorAll('.drop-zone');
+	let draggablePieces = document.querySelectorAll(".puzzle-pieces img");
 
-  let draggablePieces = piecesBoard.querySelectorAll("img")
-  //targetting ONLY piecesBoard instead of whole document
+	// arrays are indexed and start at 0
+	const imageNameArray = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
-  debugger;
+	debugger;
 
-  function switchImage(){
-    console.log(this);
-// grab the corresponding background img 0, 1, 2, or 3
-//and get it from the images folder (backGround1.jpg as an example)
-    let bgImage = `images/backGround${this.dataset.puzzleref}`;
+	function switchImage() {
+		console.log(this);
 
-// set the bg image style on the dropzone container
-    puzzleBoard.style.backgroundImage = `url(${bgImage}`;
 
+	//grab corresponding bg image (0, 1, 2, or 3)
+	//and get from images folder (backGround1.jpg as an example)
+		let bgImage = `./images/backGround${this.dataset.puzzleref}.jpg`;
+
+	//set bg image style on dropzone container
+		puzzleBoard.style.backgroundImage = `url(${bgImage})`;
+
+  //work on switching the right-hand images so that they match the buttons at the bottom
+  draggablePieces.forEach((image, index) => {
+    //log the img and the current index
+    console.log(image,index);
+
+    //change the img source
+    image.src = `images/${imageNameArray[index]}${this.dataset.puzzleref}.jpg`
     debugger;
-  }
+  });
+}
+	//debugger;
 
-  puzzleSelectors.forEach(button => button.addEventListener("click", switchImage));
 
-  //loop through the draggable images
-  // this lets us drag stuff => not that hard
-  draggablePieces.forEach(piece => {
-    piece.addEventListener("dragstart", function(e){
-      console.log("draggin...");
+	// thumbnail can be changed
+	puzzleSelectors.forEach(thumbnail => { thumbnail.addEventListener("click", switchImage) });
 
-      //dataTransfer has two methos, a setter and a getter
-      //set data on the drag, retrieve it from the drop
-      e.dataTransfer.setData("text/plain", this.id);
-    })
-  })
+	//loop throughout the draggable images
+	//draggable function
+	//this lets us drag stuff => not that hard
+	draggablePieces.forEach(piece => {
+		piece.addEventListener("dragstart", function(e) {
+			console.log('draggin...');
 
-  //this is the dragover and drop funtionality => this is for the drop zones
-  dropZones.forEach(zone => {
-    //allow user to drag over an element
-    zone.addEventListener("dragover", function(e){
-      e.preventDefault();
-      console.log('dragged sumpin over me');
-    });
+			// dataTransfer object has two methods, a seter and getter
+			// set data on the drag, and retrieve it on the drop
+			e.dataTransfer.setData("text/plain", this.id);
+		});
+	});
 
-    //allow a user to drop an element
-    zone.addEventListener("drop", function(e){
-      e.preventDefault();
-      console.log("you dropped sumpin on me");
 
-      let draggedElement = e.dataTransfer.getData("text/plain", this.id);
-      console.log('you dragged' , draggedElement);
+	//this is the dragover and drop functionality => this is for the dropzones
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", function(e) {
+			e.preventDefault();
+			console.log('dragged sumpin over me');
+		});
 
-      //add the image to the drop zone, when img is dragged over drop zone it becomes a child of the drop-zone
-      e.target.appendChild(document.querySelector(`#${draggedElement}`));
-    });
-  })
+	//allows user to drop element
+		zone.addEventListener("drop", function(e) {
+			e.preventDefault(); // don't do your default behaviou, instead do ff:
+
+			console.log('u dropped sumpin on me');
+
+			let draggedElement = e.dataTransfer.getData("text/plain");
+			console.log('you dragged:', draggedElement);
+
+			//add image to drop zone when img is dragged over drop zone it becomes a child of the drop
+			e.target.appendChild(document.querySelector(`#${draggedElement}`));
+		});
+	})
+
 })();
